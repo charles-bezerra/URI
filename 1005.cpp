5f652372b4799112f8823b29b7832e2febe77717
Anderson Kleber Dantas <anderson.dantas@clevertech.biz>
2021-10-31 19:18:48 -0300

    Add 1005

---
+++ 1005.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	double A, B;
+	cin >> A >> B;
+	
+	double MEDIA = (A * 3.5 + B * 7.5) / 11.0;
+	cout << "MEDIA = " << fixed << setprecision(5) << MEDIA << endl;
+	return 0;
+}
